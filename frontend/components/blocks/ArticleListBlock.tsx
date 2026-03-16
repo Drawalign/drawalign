d7117b2cefaabc7f3a7a97aa879bed571b9b5b18
mercurymirror <romainpruvost85@gmail.com>
2026-03-16 13:38:46 +0100

    UPDATE article list link

--- frontend/components/blocks/ArticleListBlock.tsx
+++ frontend/components/blocks/ArticleListBlock.tsx
@@ -16,29 +16,34 @@ export async function ArticleListBlock() {
       <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
         {articles.map((article) => (
           <Card key={article.id} noPadding hover>
-            {article.coverImage && (
-              <div className="relative aspect-video w-full overflow-hidden">
-                <StrapiImage
-                  image={article.coverImage}
-                  alt={article.title}
-                  fill
-                  className="object-cover"
-                />
+            <Link
+              href={`/ressources/${article.slug}`}
+              className="mt-auto self-start"
+            >
+              {article.coverImage && (
+                <div className="relative aspect-video w-full overflow-hidden">
+                  <StrapiImage
+                    image={article.coverImage}
+                    alt={article.title}
+                    fill
+                    className="object-cover"
+                  />
+                </div>
+              )}
+              <div className="flex flex-col p-6">
+                <h3 className="text-lg leading-tight lg:text-32">
+                  {article.title}
+                </h3>
+                <Link
+                  href={`/ressources/${article.slug}`}
+                  className="mt-auto self-start"
+                >
+                  <span className="flex items-center gap-2 text-black/55 text-sm tracking-tight lg:text-lg">
+                    Lire l&apos;article
+                  </span>
+                </Link>
               </div>
-            )}
-            <div className="flex flex-col p-6">
-              <h3 className="text-lg leading-tight lg:text-32">
-                {article.title}
-              </h3>
-              <Link
-                href={`/ressources/${article.slug}`}
-                className="mt-auto self-start"
-              >
-                <span className="flex items-center gap-2 text-black/55 text-sm tracking-tight lg:text-lg">
-                  Lire l&apos;article
-                </span>
-              </Link>
-            </div>
+            </Link>
           </Card>
         ))}
       </div>
