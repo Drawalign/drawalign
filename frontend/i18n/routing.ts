b39ff55b2217eb64a0f24717f89fbf9f166a5915
mercurymirror <romainpruvost85@gmail.com>
2026-04-08 16:34:42 +0200

    proxy to translate routes

--- frontend/i18n/routing.ts
+++ frontend/i18n/routing.ts
@@ -5,4 +5,19 @@ export const routing = defineRouting({
 	defaultLocale: "fr",
 	localePrefix: "as-needed",
 	localeDetection: false,
+	pathnames: {
+		"/": "/",
+		"/solutions": "/solutions",
+		"/expertises": "/expertises",
+		"/contact": "/contact",
+		"/methode-hldb": "/methode-hldb",
+		"/partenaires": {
+			fr: "/partenaires",
+			en: "/partners",
+		},
+		"/ressources": {
+			fr: "/ressources",
+			en: "/resources",
+		},
+	},
 });
