cde72f0c15ab81479aa33ad6d7125949321dc2c1
mercurymirror <romainpruvost85@gmail.com>
2026-06-30 11:37:48 +0200

    ADD actualites page

---
+++ frontend/app/[locale]/actualites/page.tsx
@@ -0,0 +1,11 @@
+export default async function ActuPage() {
+  return (
+    <div className="">
+      <iframe
+        title="Actualités"
+        src="https://actualites.draw-align.fr/widget-news.html?&style=STYLE_6&color=BLUE_DARKRED"
+        style={{ height: "700px", width: "100%", border: 0 }}
+      ></iframe>
+    </div>
+  );
+}
